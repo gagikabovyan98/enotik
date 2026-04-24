b096a27afe0c063323e372f0dac66f2dd9a78667
Михаил Мокрушин <mokrushinmix@yandex.ru>
2026-04-25 00:06:40 +0500

    Метрика

--- src/App.js
+++ src/App.js
@@ -9,22 +9,32 @@ import CollateralLoans from './pages/CollateralLoans';
 import Job from './pages/Job';
 import './App.css';
 import FooterBar from './components/FooterBar';
+import { MetrikaCounter } from 'react-metrika';
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/loans" element={<Loans />} />
-        <Route path="/cards" element={<Cards />} />
-        <Route path="/consumer-loans" element={<ConsumerLoans />} />
-        <Route path="/auto-loans" element={<AutoLoans />} />
-        <Route path="/collateral-loans" element={<CollateralLoans />} />
-        <Route path="/Job" element={<Job />} />
-      </Routes>
-      <FooterBar />
-    </BrowserRouter>
+    <>
+      <MetrikaCounter id={108751085} />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/loans" element={<Loans />} />
+          <Route path="/cards" element={<Cards />} />
+          <Route path="/consumer-loans" element={<ConsumerLoans />} />
+          <Route path="/auto-loans" element={<AutoLoans />} />
+          <Route path="/collateral-loans" element={<CollateralLoans />} />
+          <Route path="/Job" element={<Job />} />
+        </Routes>
+        <FooterBar />
+      </BrowserRouter>
+
+    </>
   );
 }
+
+
+
+
 export default App;
